94ff2aa3cc8cea7123cb07421f4952f2ad336d0d
jerbamichol-del <jerbamichol@gmail.com>
2025-12-25 13:00:33 +0100

    fix: add detailed logging to debug plugin registration

--- android/app/src/main/java/com/gestore/spese/MainActivity.java
+++ android/app/src/main/java/com/gestore/spese/MainActivity.java
@@ -2,14 +2,34 @@ package com.gestore.spese;
 
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends BridgeActivity {
+    private static final String TAG = "MainActivity";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Registra i plugin custom (ora nello stesso package)
-        registerPlugin(NotificationListenerPlugin.class);
-        registerPlugin(SMSReaderPlugin.class);
+        Log.d(TAG, "MainActivity onCreate() called");
+        
+        // Registra i plugin custom
+        try {
+            Log.d(TAG, "Registering NotificationListenerPlugin...");
+            registerPlugin(NotificationListenerPlugin.class);
+            Log.d(TAG, "NotificationListenerPlugin registered successfully!");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to register NotificationListenerPlugin", e);
+        }
+        
+        try {
+            Log.d(TAG, "Registering SMSReaderPlugin...");
+            registerPlugin(SMSReaderPlugin.class);
+            Log.d(TAG, "SMSReaderPlugin registered successfully!");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to register SMSReaderPlugin", e);
+        }
+        
+        Log.d(TAG, "All plugins registered. Bridge initialized.");
     }
 }
