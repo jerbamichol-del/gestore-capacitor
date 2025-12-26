935603366e4515947dd67756192dc4b43a1a67dd
jerbamichol-del <jerbamichol@gmail.com>
2025-12-26 13:52:01 +0100

    fix: Use Capacitor 6 plugin registration with proper plugin list

--- android-config/MainActivity.java
+++ android-config/MainActivity.java
@@ -2,24 +2,12 @@ package com.gestore.spese;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Register custom plugins with correct names
+    public void onCreate(Bundle savedInstanceState) {
         registerPlugin(SMSReaderPlugin.class);
         registerPlugin(NotificationListenerPlugin.class);
-    }
-    
-    @Override
-    public void registerPlugin(Class<? extends Plugin> pluginClass) {
-        super.registerPlugin(pluginClass);
-        
-        // Log registration for debugging
-        android.util.Log.d("MainActivity", "Registered plugin: " + pluginClass.getSimpleName());
+        super.onCreate(savedInstanceState);
     }
 }
