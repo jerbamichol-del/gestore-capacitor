3a081373d292841b220b89e6e813ad9bc22a9cbe
jerbamichol-del <jerbamichol@gmail.com>
2025-12-26 12:35:41 +0100

    fix: Register custom plugins with correct names for JavaScript

--- android-config/MainActivity.java
+++ android-config/MainActivity.java
@@ -2,16 +2,24 @@ package com.gestore.spese;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.gestore.spese.SMSReaderPlugin;
-import com.gestore.spese.NotificationListenerPlugin;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Register custom plugins
+        // Register custom plugins with correct names
         registerPlugin(SMSReaderPlugin.class);
         registerPlugin(NotificationListenerPlugin.class);
+    }
+    
+    @Override
+    public void registerPlugin(Class<? extends Plugin> pluginClass) {
+        super.registerPlugin(pluginClass);
+        
+        // Log registration for debugging
+        android.util.Log.d("MainActivity", "Registered plugin: " + pluginClass.getSimpleName());
     }
 }
